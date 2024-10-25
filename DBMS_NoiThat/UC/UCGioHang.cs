df3d6fd380f42a19fdd120e7431ce8dfa264cb42
Tran-Danh-Tan <tandanh3010@gmail.com>
2024-10-25 10:34:20 +0700

    xong tất cả các phần

--- DBMS_NoiThat/UC/UCGioHang.cs
+++ DBMS_NoiThat/UC/UCGioHang.cs
@@ -1,4 +1,5 @@
 ﻿using DBMS_NoiThat.Entity;
+using Guna.UI2.WinForms;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
@@ -25,14 +26,21 @@ namespace DBMS_NoiThat.UC
         }
         EGioHang gioHang;
         public UCGioHang(EGioHang gioHang)
-        {   
-            this.gioHang = gioHang;
-            LB_MaSanPham.Text = gioHang.MaSanPham1.ToString();
-            LB_TenSanPham.Text = gioHang.TenSanPham1;
-            LB_Gia.Text = gioHang.SoTien1.ToString();
-            TB_SoLuong.Text = gioHang.SoLuong1.ToString();
-            CK_Chon.Checked = gioHang.Check ;
+        {
             InitializeComponent();
+            this.gioHang = gioHang;
+            //LB_MaSanPham = new Label();
+            LB_MaSanPham.Text = gioHang.MaSanPham1.ToString();
+            //LB_TenSanPham = new Label();
+            LB_TenSanPham.Text = gioHang.TenSanPham1;
+            //LB_Gia = new Label();
+            LB_Gia.Text = gioHang.SoTien1.ToString();
+            //TB_SoLuong = new Guna2TextBox();
+            TB_SoLuong.Text = gioHang.SoLuong1.ToString();
+            //MessageBox.Show(LB_TenSanPham.Text);
+            //CK_Chon = new CheckBox();
+            CK_Chon.Checked = gioHang.Check ;
+
         }
 
         private void BTN_Giam_Click(object sender, EventArgs e)
@@ -61,5 +69,26 @@ namespace DBMS_NoiThat.UC
             check = CK_Chon.Checked;
             soLuong = Convert.ToInt32(TB_SoLuong.Text);
         }
+
+        private void LB_MaSanPham_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void LB_TenSanPham_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void LB_Gia_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void TB_SoLuong_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
     }
 }
