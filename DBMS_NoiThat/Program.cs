3499a220677e4c9bf839b263246f7aff3202990a
TramTieuMi <157827317+TramTieuMi@users.noreply.github.com>
2024-10-27 15:11:10 +0700

    xong chuyển form mượt đăng ký, đăng nhập

--- DBMS_NoiThat/Program.cs
+++ DBMS_NoiThat/Program.cs
@@ -1,6 +1,7 @@
 ﻿using DBMS_NoiThat.user;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics.Tracing;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -17,7 +18,7 @@ namespace DBMS_NoiThat
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new GioHang());
+            Application.Run(new TaoTaiKhoan());
         }
     }
 }
