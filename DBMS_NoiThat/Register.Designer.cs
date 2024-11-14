ee0b857c837ef5b291b94dc08cdb83f774184bc3
phungocduong <phuduong16112019@gmail.com>
2024-11-14 23:28:05 +0700

    color

--- DBMS_NoiThat/Register.Designer.cs
+++ DBMS_NoiThat/Register.Designer.cs
@@ -187,7 +187,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackColor = System.Drawing.Color.LightSteelBlue;
+            this.BackColor = System.Drawing.Color.Gray;
             this.ClientSize = new System.Drawing.Size(745, 317);
             this.Controls.Add(this.btnCancel);
             this.Controls.Add(this.btnVerifyCode);
--- DBMS_NoiThat/TaoTaiKhoan.Designer.cs
+++ DBMS_NoiThat/TaoTaiKhoan.Designer.cs
@@ -304,7 +304,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackColor = System.Drawing.Color.LightSteelBlue;
+            this.BackColor = System.Drawing.Color.Gray;
             this.ClientSize = new System.Drawing.Size(812, 437);
             this.Controls.Add(this.labelname);
             this.Controls.Add(this.btnDangKy);
